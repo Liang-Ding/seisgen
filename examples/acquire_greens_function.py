0930192f6f0b12eb25c3a1577d91a095f5cdb3d0
Liang-Ding <liangding86@gmail.com>
2022-03-09 10:21:50 -0500

    Examples to acquire the Green's function and waveform.

---
+++ examples/acquire_greens_function.py
@@ -0,0 +1,44 @@
+# -------------------------------------------------------------------
+# Acquire the Green's function from the SGT database.
+# Author: Liang Ding
+# Email: myliang.ding@mail.utoronto.ca
+# -------------------------------------------------------------------
+
+
+from seisgen.DSGTMgr import DSGTMgr
+from obspy.core.util.attribdict import AttribDict
+
+
+def acquire_greens_function():
+    '''Acquire the Green's function '''
+
+    sgt_database_folder = "path-to-the-SGT-database/"
+    model3D_folder="path-to-the-model-files(the *.bin files)/"
+    point_info_file= "path-to-the-HDF5-file-storing-the-point-location-and-interpolation-parameters(xi, eta, gamma)"
+
+    # origin example
+    origin = AttribDict({'time': '2019-07-04T18:39:44.0000Z',
+                     'latitude': 35.601333,
+                     'longitude': -117.597,
+                     'depth_in_m': 2810.0,
+                     'id': 'evt1'})
+
+    # station example: CI.SLA
+    station = AttribDict({
+        'latitude': 35.8909,
+        'longitude': -117.2833,
+        'network': 'CI',
+        'station': 'SLA',
+        'location': '',
+        'id': 'SLA'})
+
+    sgt_mgr = DSGTMgr(sgt_database_folder, model3D_folder, point_info_file)
+    print('Reading Greens functions...\n')
+    greens = sgt_mgr.get_greens_function(station, origin)
+    print(greens)
+
+
+if __name__ == '__main__':
+    acquire_greens_function()
+
+
---
+++ examples/acquire_waveform.py
@@ -0,0 +1,48 @@
+# -------------------------------------------------------------------
+# Acquire the waveform from the SGT database.
+# Author: Liang Ding
+# Email: myliang.ding@mail.utoronto.ca
+# -------------------------------------------------------------------
+
+
+from seisgen.DSGTMgr import DSGTMgr
+from obspy.core.util.attribdict import AttribDict
+import numpy as np
+
+
+def acquire_waveform():
+    '''Acquire the waveform '''
+
+    sgt_database_folder = "path-to-the-SGT-database/"
+    model3D_folder="path-to-the-model-files(the *.bin files)/"
+    point_info_file= "path-to-the-HDF5-file-storing-the-point-location-and-interpolation-parameters(xi, eta, gamma)"
+
+    # origin example
+    origin = AttribDict({'time': '2019-07-04T18:39:44.0000Z',
+                     'latitude': 35.601333,
+                     'longitude': -117.597,
+                     'depth_in_m': 2810.0,
+                     'id': 'EVT1'})
+
+    # station example: CI.SLA
+    station = AttribDict({
+        'latitude': 35.8909,
+        'longitude': -117.2833,
+        'network': 'CI',
+        'station': 'SLA',
+        'location': '',
+        'id': 'SLA'})
+
+    sgt_mgr = DSGTMgr(sgt_database_folder, model3D_folder, point_info_file)
+
+    # moment tensor example [Mrr, Mtt, Mpp, Mrt, Mrp, Mtp]
+    mt_RTP = np.array([8.6E20, -1.3E22, 1.2E22, -2.1E21, -3.2E21, 6.8E21])
+
+    print('Reading waveform ...\n')
+    st = sgt_mgr.get_waveform(station, origin, mt_RTP)
+    print(st)
+
+
+if __name__ == '__main__':
+    acquire_waveform()
+
