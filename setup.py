74164cb222156ef034eda0707911ad799a0571b0
Liang Ding <liangding86@gmail.com>
2022-03-22 15:17:39 -0400

    Update version to 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="seisgen",
-    version="0.0.1",
+    version="0.0.2",
     author="Liang Ding",
     author_email="myliang.ding@mail.utoronto.ca",
     description="The theoretical seismic waveform generation tool",
